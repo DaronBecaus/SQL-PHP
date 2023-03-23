967cf2e7a6244befd1695b61fd45995e01ccc596
DaronBecaus <126109661+DaronBecaus@users.noreply.github.com>
2023-03-22 22:03:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -46,12 +46,28 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "SELECT * FROM pessoas";
     $resultado = $conn->query($sql);
-    var_dump($resultado);
-    echo "--------------";
+    // var_dump($resultado);
+    // echo "--------------";
     $lista = $resultado->fetchAll();
-    var_dump($lista);
+    // var_dump($lista);
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
 
-echo "<br>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>

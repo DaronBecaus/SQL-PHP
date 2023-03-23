295f66cf9575fece5af7190dbf587c3b3c1e08a0
DaronBecaus <126109661+DaronBecaus@users.noreply.github.com>
2023-03-23 00:36:23 -0300

    gitignore

---
+++ deletar.php
@@ -0,0 +1,10 @@
+<?php
+
+$id_pessoa = $_GET['id'];
+try {
+    $pessoa = new Pessoa($id_pessoa);
+    $pessoa->deletar();
+    header('location: index.php');
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
--- pessoas.php
+++ pessoas.php
@@ -12,6 +12,26 @@ class Pessoas
     // UPDATE = atualizar
     // DELETe = deletar
 
+    public function __construct($id_pessoa = false)
+    {
+        if ($id_pessoa) {
+            $this->id_pessoa = $id_pessoa;
+            $this->carregar();
+        }
+    }
+
+    public function carregar()
+    {
+        $sql = "SELECT nome, idade FROM pessoas WHERE id_pessoa = :id_pessoa";
+        $conexao = Conexao::conectar();
+        $stmt = $conexao->prepare($sql);
+        $stmt->bindValue(":id_pessoa", $this->id_pessoa);
+        $stmt->execute();
+        $lista = $stmt->fetch();
+        $this->nome = $lista['nome'];
+        $this->idade = $lista['idade'];
+    }
+
     public function deletar()
     {
         $sql = "DELETE FROM pessoas WHERE id_pessoa = :id_pessoa";

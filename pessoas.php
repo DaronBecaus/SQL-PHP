a2869cabd83f3a382353aa9caf458f484d1ab042
DaronBecaus <danielsilvasoder@gmail.com>
2023-03-23 21:58:33 -0300

    Update pessoas.php

--- pessoas.php
+++ pessoas.php
@@ -32,6 +32,30 @@ class Pessoas
         $this->idade = $lista['idade'];
     }
 
+    public function inserir()
+    {
+        $sql = "INSERT INTO pessoas (nome, idade) VALUES (:nome, :idade)";
+        $conexao = Conexao::conectar();
+        $stmt = $conexao->prepare($sql);
+        $stmt->bindValue(":nome", $this->nome);
+        $stmt->bindValue(":idade", $this->idade);
+        $stmt->execute();
+    }
+
+    public static function listar()
+    {
+        $sql = "SELECT * FROM pessoas";
+        $conexao = Conexao::conectar();
+        $resultado = $resultado->fetchAll();
+        return $lista;
+    }
+
+    public function atualizar()
+    {
+        $sql = "UPDATE pessoas SET nome = :nome, idade = :idade WHERE id_pessoa = :id_pessoa"
+        $conexao = Conexao::conectar();
+    }
+
     public function deletar()
     {
         $sql = "DELETE FROM pessoas WHERE id_pessoa = :id_pessoa";
